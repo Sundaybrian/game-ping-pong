14bd240f41f4df426de364777e4c0a09254ba093
sunday brian <brians931@gmail.com>
2017-08-18 18:09:29 +0300

    Add count up function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
       event.preventDefault();
+
       var number = parseInt($("input#number").val());
+      counter()
     console.log (number);
     });
   });
+
+var counter= function (number) {
+  for (var i=1; i<=16; i++){
+     if ((i%3===0)&& (i%5===0)) {
+      alert("ping pong");
+
+    }else if (i%5===0) {
+      alert("pong");
+
+    }else if (i%3===0){
+
+      alert('ping');
+    }else {
+      alert(i);
+    }
+
+
+  }
+
+
+};
